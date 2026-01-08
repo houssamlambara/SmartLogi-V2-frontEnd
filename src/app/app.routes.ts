221fe158e3d13e2aa09cb1315cc0e6b760465e9f
houssamlambara <lambarahoussam@gmail.com>
2026-01-08 21:50:18 +0100

    Configuration des routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // {
+    //     path: 'dashboard',
+    //     redirectTo: 'dashboard'
+    //     pathMatch: 'full'
+    // },
+    {
+        path: 'auth',
+        loadChildren: () => import('./features/auth/auth.routes').then(m => m.AUTH_ROUTES)
+    },
+    // {
+    //     path: 'dashboard',
+    //     loadChildren: () => import('./features/dashboard/dashboard.routes').then(m => m.DASHBOARD_ROUTES),
+    //     canActivate: [authGuard]
+    // },
+    // {
+    //     path: 'parcels',
+    //     loadChildren: () => import('./features/parcel/parcel.routes').then(m => m.PARCEL_ROUTES),
+    //     canActivate: [authGuard]
+    // },
+    // {
+    //     path: 'tracking',
+    //     loadChildren: () => import('./features/tracking/tracking.routes').then(m => m.TRACKING_ROUTES)
+    // },
+    // {
+    //     path: '**',
+    //     redirectTo: 'dashboard'
+    // }
+];
