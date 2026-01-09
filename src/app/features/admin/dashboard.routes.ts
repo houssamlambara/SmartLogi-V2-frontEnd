2826ea48a747d75f858b05f6545c312629100750
houssamlambara <lambarahoussam@gmail.com>
2026-01-09 15:01:59 +0100

    Ajout du module admin avec dashboard

---
+++ src/app/features/admin/dashboard.routes.ts
@@ -0,0 +1,12 @@
+import { Route, Routes } from '@angular/router';
+import { AuthGuard } from '../../core/guards/auth.guard';
+import { RoleGuard } from '../../core/guards/role.guard';
+
+export const DASHBOARD_ROUTES: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: (() => import("./dashboard/admin-dashboard").then(d => d.AdminDashboard)),
+    canActivate: [AuthGuard, RoleGuard],
+    data: { role: 'GESTIONNAIRE' },
+  },
+];
---
+++ src/app/features/admin/dashboard/admin-dashboard.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-dashboard',
+  imports: [],
+  standalone: true,
+  templateUrl: './admin-dashboard.html',
+  styleUrls: ['./admin-dashboard.css']
+})
+export class AdminDashboard {
+
+}
