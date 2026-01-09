86fcc958fd0b28fe802d9d8acf712a172f951827
houssamlambara <lambarahoussam@gmail.com>
2026-01-09 15:06:58 +0100

    Ajout de la navbar et des imports partagés

---
+++ src/app/shared/components/navbar/navbar/navbar.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../../../core/services/auth.service';
+import { toast } from 'ngx-sonner';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [NgIf, RouterLink, RouterLinkActive],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css',
+  standalone: true,
+})
+export class NavbarComponent {
+  menuOpen = false;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
+  }
+
+  hasRole(role: string): boolean {
+    return this.authService.hasRole(role);
+  }
+
+  logout() {
+    this.authService.logout();
+    toast.success("Logout successfuly");
+    this.router.navigate(['/auth/login']);
+  }
+}
