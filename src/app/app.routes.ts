c37195035077b55b580e85af310ca855967f74cf
houssamlambara <lambarahoussam@gmail.com>
2026-01-15 00:19:53 +0100

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,47 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
+import { RoleGuard } from './core/guards/role.guard';
 
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import("./pages/home/home.page").then(h => h.Home),
+    loadComponent: () => import('./pages/home/home.page').then((h) => h.Home),
   },
   {
     path: 'auth',
-    loadChildren: () => import('./features/auth/auth.routes').then(m => m.AUTH_ROUTES),
+    loadChildren: () => import('./features/auth/auth.routes').then((m) => m.AUTH_ROUTES),
   },
   {
     path: 'unauthorized',
-    loadComponent: () => import('./features/auth/pages/unauthorized/unauthorized').then(u => u.Unauthorized),
+    loadComponent: () =>
+      import('./features/auth/pages/unauthorized/unauthorized').then((u) => u.Unauthorized),
   },
   {
     path: 'admin',
-    loadChildren: () => import('./features/admin/dashboard.routes').then(m => m.DASHBOARD_ROUTES),
+    loadChildren: () => import('./features/admin/dashboard.routes').then((m) => m.DASHBOARD_ROUTES),
+  },
+  {
+    path: 'colis',
+    loadComponent: () => import('./features/colis/pages/colis/colis').then((c) => c.Colis),
+    canActivate: [AuthGuard, RoleGuard],
+    data: {
+      roles: ['Sender', 'GESTIONNAIRE', 'LIVREUR'],
+    },
+  },
+  {
+    path: 'oauth2/callback',
+    loadComponent: () => import("./features/auth/components/OAuth2CallbackComponent").then(o => o.OAuth2CallbackComponent),
+  },
+  {
+    path: "colis/:id",
+    loadComponent: () => import("./features/colis/pages/colis-precise/single-colis/single-colis").then(c => c.SingleColis),
+    canActivate: [AuthGuard, RoleGuard],
+    data: {
+      roles: ['Sender', 'GESTIONNAIRE', 'LIVREUR'],
+    },
   },
   {
     path: '**',
-    loadComponent: () => import("./pages/not-found/not-found.page").then(n => n.NotFound),
+    loadComponent: () => import('./pages/not-found/not-found.page').then((n) => n.NotFound),
   },
 ];
