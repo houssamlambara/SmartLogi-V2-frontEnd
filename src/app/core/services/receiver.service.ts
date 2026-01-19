26927e2f3473015b0b61a5e858a13a209e0f0f17
houssamlambara <lambarahoussam@gmail.com>
2026-01-19 16:42:08 +0100

    ajout de la gestion des destinataires

---
+++ src/app/core/services/receiver.service.ts
@@ -0,0 +1,24 @@
+import { inject, Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { map, Observable } from 'rxjs';
+import { ReceiverModel } from '../../features/receivers/models/receiver.model';
+import { environment } from '../../environment/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class receviersService {
+  private apiSer = inject(ApiService);
+
+  getReceivers(): Observable<ReceiverModel[]> {
+    return this.apiSer.get<ReceiverModel[]>(`destinations`).pipe(
+      map((resp: any) => {
+        return resp.data.content || resp.data;
+      })
+    );
+  }
+
+  createReceiver(receiver: any): Observable<any> {
+    return this.apiSer.post(`destinations`, receiver);
+  }
+}
---
+++ src/app/features/receivers/models/new-receiver.model.ts
@@ -0,0 +1,7 @@
+export interface newReceiverModel {
+  nom: string;
+  prenom: string;
+  email: string;
+  telephone: string;
+  adresse: string;
+}
