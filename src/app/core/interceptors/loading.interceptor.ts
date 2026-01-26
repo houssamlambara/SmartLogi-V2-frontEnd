b2a67ce6142eb566044cb5234853f168cb384122
houssam <lambarahoussam@gmail.com>
2026-01-26 09:15:23 +0100

    Intercepteur de chargement

--- src/app/core/interceptors/loading.interceptor.ts
+++ src/app/core/interceptors/loading.interceptor.ts
@@ -1,14 +1,28 @@
-import { HttpInterceptorFn } from "@angular/common/http";
-import { inject } from "@angular/core";
-import { NgxSpinnerService } from "ngx-spinner";
-import { finalize } from "rxjs";
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { Store } from '@ngrx/store';
+import { finalize } from 'rxjs';
+import { showLoading, hideLoading } from '../../store/ui/ui.actions';
 
 export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
-    const spinnerService = inject(NgxSpinnerService);
+  const spinnerService = inject(NgxSpinnerService);
+  const store = inject(Store);
+
+  const excludedUrls = ['/api/health', '/api/ping'];
+  const shouldShowLoading = !excludedUrls.some((url) => req.url.includes(url));
+
+  if (shouldShowLoading) {
     spinnerService.show();
-    return next(req).pipe(
-        finalize(() => {
-            spinnerService.hide();
-        })
-    );
-}
+    store.dispatch(showLoading());
+  }
+
+  return next(req).pipe(
+    finalize(() => {
+      if (shouldShowLoading) {
+        spinnerService.hide();
+        store.dispatch(hideLoading());
+      }
+    })
+  );
+};
