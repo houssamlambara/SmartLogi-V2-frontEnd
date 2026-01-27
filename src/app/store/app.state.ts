950ab7108f54b0e6a46e3f5571f12ea6205467fa
houssam <lambarahoussam@gmail.com>
2026-01-27 20:22:12 +0100

    simplification du store ngrx

--- src/app/store/app.state.ts
+++ src/app/store/app.state.ts
@@ -1,15 +1,7 @@
 import { AuthState } from './auth/auth.state';
 import { ColisState } from './colis/colis.state';
-import { LivreursState } from './livreurs/livreurs.state';
-import { SendersState } from './senders/senders.state';
-import { ReceiversState } from './receivers/receivers.state';
-import { UIState } from './ui/ui.state';
 
 export interface AppState {
   auth: AuthState;
   colis: ColisState;
-  livreurs: LivreursState;
-  senders: SendersState;
-  receivers: ReceiversState;
-  ui: UIState;
 }
