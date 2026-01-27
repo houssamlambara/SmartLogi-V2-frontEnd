238406824c22225b95888e72e3eda900fcbee3a9
houssam <lambarahoussam@gmail.com>
2026-01-27 20:23:31 +0100

    mise a jour du resolver livreurs

--- src/app/core/resolvers/livreurs.resolver.ts
+++ src/app/core/resolvers/livreurs.resolver.ts
@@ -1,27 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
-import { filter, take, tap } from 'rxjs/operators';
-import { Store } from '@ngrx/store';
-import { Livreur } from '../../store/livreurs/livreurs.state';
-import { selectAllLivreurs } from '../../store/livreurs/livreurs.selectors';
-import { loadLivreurs } from '../../store/livreurs/livreurs.actions';
+import { livreurService } from '../services/livreurs.service';
+import { livreurModel } from '../models/livreurs.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class LivreursResolver implements Resolve<Livreur[]> {
-  constructor(private store: Store) {}
+export class LivreursResolver implements Resolve<livreurModel[]> {
+  constructor(private livreurService: livreurService) {}
 
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Livreur[]> {
-    return this.store.select(selectAllLivreurs).pipe(
-      tap((livreurs) => {
-        if (livreurs.length === 0) {
-          this.store.dispatch(loadLivreurs());
-        }
-      }),
-      filter((livreurs) => livreurs.length > 0),
-      take(1)
-    );
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<livreurModel[]> {
+    return this.livreurService.getLivreurs();
   }
 }
